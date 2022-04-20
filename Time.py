5cb7259cf73d891adc081de14d2871fe336324d7
fatemi127 <fatemi127@gmail.com>
2022-04-21 00:09:28 +0430

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,20 @@
+
+class time:
+    def __init__(self,h,m,s):
+        self.hour = h 
+        self.minutes = m
+        self.seconds = s
+    def sub (self):
+        pass
+    def mul (self):
+        pass
+    def convert_to_time (self):
+        pass
+    def convert_to_seconds (self):
+        pass
+
+nemooneh = time (4,2,3)
+nemooneh.sub()
+nemooneh.mul()
+nemooneh.convert_to_time()
+nemooneh.convert_to_seconds()
